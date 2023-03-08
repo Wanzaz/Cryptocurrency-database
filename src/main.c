1cc26f303169e134869f28fcebebd35438ed1887
Wanzaz <ondra.pazourek@gmail.com>
2023-03-08 13:14:10 +0100

    Add main.c with structures

---
+++ src/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>  // for work with text strings
+#include <stdbool.h> // for work with type of bool and constants true and false
+// #include <ctype.h>   // isalpha, isspace, islower, isupper, ...
+#include <math.h>    // functions from math library
+// #include <float.h>   // constants for racional types DBL_MAX, DBL_DIG, ...
+// #include <limits.h>  // constants for integer types INT_MAX, INT_MIN, ...
+#include <time.h>    // function time and other for work with time
+#include <errno.h>
+
+typedef struct
+{
+    char name[41];
+    char founder_name[41];
+    int price;
+    int foundation_year;
+} TCryptocurrency;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+    
+
+    return 0;
+}
