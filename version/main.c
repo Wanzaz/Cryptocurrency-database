af0273df6b35b1a8783ac5e25ce28d6131929929
Wanzaz <ondra.pazourek@gmail.com>
2023-03-22 19:55:52 +0100

    Change to it's right format + done version 1

--- version/main.c
+++ version/main.c
@@ -118,179 +118,169 @@ void writeCryptocurrencies(FILE *output, TArrayOfCrypto * crypto)
 
 /*************** SORTING FUNCTIONS - BY YEAR AND LENGTH OF NAME ***************/
 
-/* // function to swap elements */
-/* void swap(TCryptocurrency array[], int x, int y) */
-/* { */
-/* 	TCryptocurrency temp = array[x]; */
-/* 	array[x] = array[y]; */
-/* 	array[y] = temp; */
-/* } */
+// function to swap elements
+void swap(TCryptocurrency array[], int x, int y)
+{
+	TCryptocurrency temp = array[x];
+	array[x] = array[y];
+	array[y] = temp;
+}
 
-/* // function to find the partition position */
-/* static inline */
-/* int partition(TCryptocurrency array[], int start, int end) */
-/* { */
-/* 	int p = (start + end)/2; // or other selection */
-/* 	TCryptocurrency pivot = array[p]; */
-/* 	swap(array, p, start); // pivot removal on site */
+// function to find the partition position
+static inline
+int partition(TCryptocurrency array[], int start, int end)
+{
+	int p = (start + end)/2; // or other selection
+	TCryptocurrency pivot = array[p];
+	swap(array, p, start); // pivot removal on site
 
-/* 	int left = start; */
-/* 	int right = end + 1; */
+	int left = start;
+	int right = end + 1;
 
-/* 	while (true) { */
-/* 		while (array[++left].foundation_year < pivot.foundation_year) { */
-/* 			if (left == end) break; // find element >= pivot */
-/* 		} */
-/* 		while (pivot.foundation_year < array[--right].foundation_year) { */
-/* 			// this line doesn't have to be here - it shouldn't be executed in any time */
-/* 			/1* if (left == end) break; // find element >= pivot *1/ */ 
-/* 		} */
+	while (true) {
+		while (array[++left].foundation_year < pivot.foundation_year) {
+			if (left == end) break; // find element >= pivot
+		}
+		while (pivot.foundation_year < array[--right].foundation_year) {
+			// this line doesn't have to be here - it shouldn't be executed in any time
+			/* if (left == end) break; // find element >= pivot */ 
+		}
 
-/* 		if (left >= right) break; */
+		if (left >= right) break;
 
-/* 		swap(array,left, right); */
-/* 	} // while */
+		swap(array,left, right);
+	} // while
 	
-/* 	swap(array, right, start); // inserting pivot on final position */
-/* 	return right; */
-/* } */
+	swap(array, right, start); // inserting pivot on final position
+	return right;
+}
 
-/* void _quickSort(TCryptocurrency array[], int start, int end) */
-/* { */
-/* 	if (start >= end) return; */
+void quickSort(TCryptocurrency array[], int start, int end)
+{
+	if (start >= end) return;
 
-/* 	int pivot_position = partition(array, start, end); */
+	int pivot_position = partition(array, start, end);
 
-/* 	_quickSort(array, start, pivot_position - 1); */
-/* 	_quickSort(array, pivot_position + 1, end); */
-/* } */
+	quickSort(array, start, pivot_position - 1);
+	quickSort(array, pivot_position + 1, end);
+}
 
-/* void quickSort(TCryptocurrency array[], int n) */
-/* { */
-/* 	_quickSort(array, 0, n - 1); */
+bool isSortedByFoundationYear(TArrayOfCrypto *crypto)
+{
+    for (int i = 0; i < crypto->lenght - 1; i++) {
+        if (crypto->value[i].foundation_year > crypto->value[i + 1].foundation_year) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void sortByFoundationYear(FILE *output, TArrayOfCrypto *crypto)
+{
+    quickSort(crypto->value, 0, crypto->lenght - 1);
+    rewind(output);
+    writeCryptocurrencies(output, crypto);
+    writeCryptocurrencies(stdout, crypto);
+    printf("\n\n[DATABASE SORTED BY A FOUNDATION YEAR]: %s\n", isSortedByFoundationYear(crypto) ? "YES" : "NO");
+}
+
+
+static inline
+int partitionString(TCryptocurrency array[], int start, int end)
+{
+	int p = (start + end)/2; // or other selection
+	TCryptocurrency pivot = array[p];
+	swap(array, p, start); // pivot removal on site
+
+	int left = start;
+	int right = end + 1;
+
+	while (true) {
+		while (strlen(array[++left].founder_name) < strlen(pivot.founder_name)) {
+			if (left == end) break; // find element >= pivot
+		}
+		while (strlen(pivot.founder_name) < strlen(array[--right].founder_name)) {
+			// this line doesn't have to be here - it shouldn't be executed in any time
+			/* if (left == end) break; // find element >= pivot */ 
+		}
+
+		if (left >= right) break;
+
+		swap(array,left, right);
+	} // while
 	
-/* } */
+	swap(array, right, start); // inserting pivot on final position
+	return right;
+}
 
-/* bool isSortedByFoundationYear(TCryptocurrency array[], int length) */
-/* { */
-/*     for (int i = 0; i < length - 1; i++) { */
-/*         if (array[i].foundation_year > array[i + 1].foundation_year) { */
-/*             return false; */
-/*         } */
-/*     } */
-/*     return true; */
-/* } */
+void quickSortString(TCryptocurrency array[], int start, int end)
+{
+	if (start >= end) return;
 
-/* void sortByFoundationYear(FILE *output, TCryptocurrency array[], int length) */
-/* { */
-/*     quickSort(array, length); */
-/*     rewind(output); */
-/*     write(output, array, length); */
-/*     write(stdout, array, length); */
-/*     printf("\n\n[DATABASE SORTED BY A FOUNDATION YEAR]: %s\n", isSortedByFoundationYear(array, length) ? "YES" : "NO"); */
-/* } */
+	int pivot_position = partitionString(array, start, end);
 
-/* static inline */
-/* int partitionString(TCryptocurrency array[], int start, int end) */
-/* { */
-/* 	int p = (start + end)/2; // or other selection */
-/* 	TCryptocurrency pivot = array[p]; */
-/* 	swap(array, p, start); // pivot removal on site */
+	quickSortString(array, start, pivot_position - 1);
+	quickSortString(array, pivot_position + 1, end);
+}
 
-/* 	int left = start; */
-/* 	int right = end + 1; */
 
-/* 	while (true) { */
-/* 		while (strlen(array[++left].founder_name) < strlen(pivot.founder_name)) { */
-/* 			if (left == end) break; // find element >= pivot */
-/* 		} */
-/* 		while (strlen(pivot.founder_name) < strlen(array[--right].founder_name)) { */
-/* 			// this line doesn't have to be here - it shouldn't be executed in any time */
-/* 			/1* if (left == end) break; // find element >= pivot *1/ */ 
-/* 		} */
+bool isSortedByFounderNameLength(TArrayOfCrypto *crypto)
+{
+    for (int i = 0; i < crypto->lenght - 1; i++) {
+        if (strlen(crypto->value[i].founder_name) > strlen(crypto->value[i + 1].founder_name)) {
+            return false;
+        }
+    }
+    return true;
+}
 
-/* 		if (left >= right) break; */
-
-/* 		swap(array,left, right); */
-/* 	} // while */
-	
-/* 	swap(array, right, start); // inserting pivot on final position */
-/* 	return right; */
-/* } */
-
-/* void _quickSortString(TCryptocurrency array[], int start, int end) */
-/* { */
-/* 	if (start >= end) return; */
-
-/* 	int pivot_position = partitionString(array, start, end); */
-
-/* 	_quickSortString(array, start, pivot_position - 1); */
-/* 	_quickSortString(array, pivot_position + 1, end); */
-/* } */
-
-/* void quickSortString(TCryptocurrency array[], int n) */
-/* { */
-/* 	_quickSortString(array, 0, n - 1); */
-	
-/* } */
-
-/* bool isSortedByFounderNameLength(TCryptocurrency array[], int length) */
-/* { */
-/*     for (int i = 0; i < length - 1; i++) { */
-/*         if (strlen(array[i].founder_name) > strlen(array[i + 1].founder_name)) { */
-/*             return false; */
-/*         } */
-/*     } */
-/*     return true; */
-/* } */
-
-/* void sortByFounderNameLength(FILE *output, TCryptocurrency array[], int length) */
-/* { */
-/*     quickSortString(array, length); */
-/*     write(output, array, length); */
-/*     printf("\n\n[DATABASE SORTED BY THE FOUNDER NAME'S LENGTH]: %s\n", isSortedByFounderNameLength(array, length) ? "YES" : "NO"); */
-/* } */
+void sortByFounderNameLength(FILE *output, TArrayOfCrypto *crypto)
+{
+    quickSortString(crypto->value, 0, crypto->lenght - 1);
+    writeCryptocurrencies(output, crypto);
+    printf("\n\n[DATABASE SORTED BY THE FOUNDER NAME'S LENGTH]: %s\n", isSortedByFounderNameLength(crypto) ? "YES" : "NO");
+}
 
 
 
 /*************** SUMMARY ***************/
 
-/* int averageFoundationYear(TCryptocurrency array[], int n) */
-/* { */
-/*     int sum = 0; */
-/*     for (int i = 0; i < n - 1; i++) { */
-/*         sum += array[i].foundation_year; */
-/*     } */
+int averageFoundationYear(TArrayOfCrypto *crypto)
+{
+    int sum = 0;
+    for (int i = 0; i < crypto->lenght - 1; i++) {
+        sum += crypto->value[i].foundation_year;
+    }
 
-/*     return sum/(n - 1); */
-/* } */
+    return sum/(crypto->lenght - 1);
+}
 
-/* void summary(TCryptocurrency array[], int n) */
-/* { */
-/*     int average = averageFoundationYear(array, n); */
-/*     int youngest = array[0].foundation_year; */
-/*     int oldest = array[n - 1].foundation_year; */
-/*     printf("[SUMMARY]:\n" */
-/*            "The number of records: %d\n" */
-/*            "The average year of foundation: %d\n" */
-/*            "The youngest cryptocurrency was founded in year: %d\n" */
-/*            "The oldest cryptocurrency was founded in year: %d\n" */
-/*            , n, average, youngest, oldest); */
-/* } */
+void summary(TArrayOfCrypto *crypto)
+{
+    int average = averageFoundationYear(crypto);
+    int youngest = crypto->value[0].foundation_year;
+    int oldest = crypto->value[crypto->lenght - 1].foundation_year;
+    printf("[SUMMARY]:\n"
+           "The number of records: %d\n"
+           "The average year of foundation: %d\n"
+           "The youngest cryptocurrency was founded in year: %d\n"
+           "The oldest cryptocurrency was founded in year: %d\n"
+           , crypto->lenght, average, youngest, oldest);
+}
 
 
 
 /*************** FILTER FUNCTION ***************/
 
-/* void theOGs(FILE *output, TCryptocurrency array[], int n) */
-/* { */
-/*     printf("[THE OG CRYPTOCURRENCIES WHICH WERE FOUNDED BEFORE YEAR 2014]:\n"); */
-/*     for (int i = 0; i < n - 1; i++) { */
-/*         if (array[i].foundation_year < 2014) { */
-/*             fprintf(output, " %s\n", array[i].name); */
-/*         } */
-/*     } */
-/* } */
+void theOGs(FILE *output, TArrayOfCrypto *crypto)
+{
+    printf("[THE OG CRYPTOCURRENCIES WHICH WERE FOUNDED BEFORE YEAR 2014]:\n");
+    for (int i = 0; i < crypto->lenght - 1; i++) {
+        if (crypto->value[i].foundation_year < 2014) {
+            fprintf(output, " %s\n", crypto->value[i].name);
+        }
+    }
+}
 
 
 
@@ -327,8 +317,6 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    /* TCryptocurrency* array = (TCryptocurrency*) malloc(sizeof(TCryptocurrency) * MAX); */
-
     char *inputpath = argv[1];
     FILE *database = fopen(inputpath, "r+");
     if (database == NULL) return -1;
@@ -336,63 +324,52 @@ int main(int argc, char *argv[])
     TArrayOfCrypto * crypto = loadCryptocurrencies(database);
     if (crypto == NULL) return -2;
 
-    loadCryptocurrencies(database);
-    writeCryptocurrencies(stdout, crypto);
 
+    int choice = 1;
+
+    while (choice != 0) {
+        mainMenu();
+        scanf("%d", &choice);
+        
+        switch(choice) {
+            case 1: clear();
+                writeCryptocurrencies(stdout, crypto);
+                break;
+            /* case 2: clear(); */
+            /*     break; */
+            /* case 3: clear(); */
+            /*     break; */
+            /* case 4: clear(); */
+            /*     break; */
+            case 5: clear();
+                summary(crypto);
+                break;
+            case 6: clear();
+                sortByFoundationYear(database, crypto);
+                break;
+            case 7: clear();
+                sortByFounderNameLength(stdout, crypto);
+                break;
+            case 8: clear();
+                    theOGs(stdout, crypto);
+                break;
+        }
+        
+        if (choice != 0 && choice != 1 && choice != 2 && choice != 3 && choice != 4 && choice != 5 && choice != 6 && choice != 7 && choice != 8) {
+            printf("\n[ERROR]: Uknown operation\n"
+                   " Exiting the program...\n"
+                    );
+            exit(-1);
+        }
+
+        if (choice != 0) {
+            pause();
+        }
+    }
+
+    fclose(database);
     free(crypto->value);
     free(crypto);
-
-
-
-
-
-    /* int n = load(database, array, MAX); */
-
-
-    /* int choice = 1; */
-
-    /* while (choice != 0) { */
-    /*     mainMenu(); */
-    /*     scanf("%d", &choice); */
-        
-    /*     switch(choice) { */
-    /*         case 1: clear(); */
-    /*             write(stdout, array, n); */
-    /*             break; */
-    /*         /1* case 2: clear(); *1/ */
-    /*         /1*     break; *1/ */
-    /*         /1* case 3: clear(); *1/ */
-    /*         /1*     break; *1/ */
-    /*         /1* case 4: clear(); *1/ */
-    /*         /1*     break; *1/ */
-    /*         case 5: clear(); */
-    /*             summary(array, n); */
-    /*             break; */
-    /*         case 6: clear(); */
-    /*             sortByFoundationYear(database, array, n); */
-    /*             break; */
-    /*         case 7: clear(); */
-    /*             sortByFounderNameLength(stdout, array, n); */
-    /*             break; */
-    /*         case 8: clear(); */
-    /*                 theOGs(stdout, array, n); */
-    /*             break; */
-    /*     } */
-        
-    /*     if (choice != 0 && choice != 1 && choice != 2 && choice != 3 && choice != 4 && choice != 5 && choice != 6 && choice != 7 && choice != 8) { */
-    /*         printf("\n[ERROR]: Uknown operation\n" */
-    /*                " Exiting the program...\n" */
-    /*                 ); */
-    /*         exit(-1); */
-    /*     } */
-
-    /*     if (choice != 0) { */
-    /*         pause(); */
-    /*     } */
-    /* } */
-
-    /* fclose(database); */
-    /* free(array); */
 
     return 0;
 }
